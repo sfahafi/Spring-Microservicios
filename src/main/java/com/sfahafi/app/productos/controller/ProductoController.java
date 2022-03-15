4b8e8ed894925e038a8db0eaf7d95dbc301bb5eb
ubuntu <ubuntu@ubuntu-Lenovo-B40-70>
2022-03-14 21:18:34 -0300

    Agregado Eureka Server, puertos dinamicos y otras config

--- src/main/java/com/sfahafi/app/productos/controller/ProductoController.java
+++ src/main/java/com/sfahafi/app/productos/controller/ProductoController.java
@@ -1,8 +1,11 @@
 package com.sfahafi.app.productos.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +17,28 @@ import com.sfahafi.app.productos.service.IProductoService;
 public class ProductoController {
 	
 	@Autowired
+	private Environment env;
+	
+	@Value("${server.port}")
+	private Integer port;
+	
+	@Autowired
 	private IProductoService ips;
 	
 	@GetMapping("/listar")
 	public List<Producto> listar(){
-		return ips.buscarTodos();
+		return ips.buscarTodos().stream().map(producto ->{
+			//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			producto.setPort(port); //otra forma de traer parametros del archivo de configuracion con la anotacion @Value
+			return producto;
+		}).collect(Collectors.toList());
 	}
 	
-	@GetMapping("/buscar/{id}")
+	@GetMapping("/ver/{id}")
 	public Producto detalle(@PathVariable Long id) {
-		return ips.buscarPorId(id);
+		Producto producto = ips.buscarPorId(id);
+		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		return producto;
 	}
 
 }
--- src/main/java/com/sfahafi/app/productos/models/entity/Producto.java
+++ src/main/java/com/sfahafi/app/productos/models/entity/Producto.java
@@ -26,6 +26,8 @@ public class Producto implements Serializable{
 	@Temporal(TemporalType.DATE)
 	private Date createAt;   
 	
+	@Transient
+	private Integer port;
 	
 	public Long getId() {
 		return id;
@@ -50,6 +52,12 @@ public class Producto implements Serializable{
 	}
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
+	}
+	public Integer getPort() {
+		return port;
+	}
+	public void setPort(Integer port) {
+		this.port = port;
 	}
 	
 	
